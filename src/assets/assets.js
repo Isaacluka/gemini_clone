3fa54ac660a5b9c2e5ab3cfd13a1ab86da939f47
Isaacluka <lukaisaacabdulkarim@gmail.com>
2025-10-07 16:12:12 +0100

    User Interface Completed

---
+++ src/assets/assets.js
@@ -0,0 +1,31 @@
+import plus_icon from './add.png'
+import bulb_icon from './bulb.png'
+import compass_icon from './compass.png'
+import menu_icon from './hamburger.png'
+import mic_icon from './microphone.png'
+import gallery_icon from './photo.png'
+import youtube_icon from './youtube.png'
+import question_icon from './question.png'
+import setting_icon from './settings.png'
+import message_icon from './chat.png'
+import send_icon from './send.png'
+import code_icon from './coding.png'
+import user_icon from './Isaac.png'
+import history_icon from './history.png'
+
+export const assets = {
+    plus_icon,
+    bulb_icon,
+    compass_icon,
+    menu_icon,
+    mic_icon,
+    gallery_icon,
+    youtube_icon,
+    question_icon,
+    setting_icon,
+    message_icon,
+    send_icon,
+    code_icon,
+    user_icon,
+    history_icon
+}
